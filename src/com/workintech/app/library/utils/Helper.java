25e715866206ff965ea8219da4d7442bea4aae72
Mehmet Ali Değirmenci <86076087+malidegirmenci@users.noreply.github.com>
2023-12-08 02:52:51 +0300

    check value for null

--- src/com/workintech/app/library/utils/Helper.java
+++ src/com/workintech/app/library/utils/Helper.java
@@ -3,9 +3,16 @@ package com.workintech.app.library.utils;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
-public class Helper {
+public class Helper{
     public static LocalDate convertLD(String date) {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
         return LocalDate.parse(date, formatter);
+    }
+
+    public static boolean isNull(String value) {
+        return value == null;
+    }
+    public static boolean isNull(Integer value) {
+        return value == null;
     }
 }
