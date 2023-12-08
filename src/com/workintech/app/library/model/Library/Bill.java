33078ed1afaeba125678964060c1b1e139bab82d
Mehmet Ali Değirmenci <86076087+malidegirmenci@users.noreply.github.com>
2023-12-08 15:12:38 +0300

    overrided toString and compareTo methods

--- src/com/workintech/app/library/model/Library/Bill.java
+++ src/com/workintech/app/library/model/Library/Bill.java
@@ -1,20 +1,18 @@
 package com.workintech.app.library.model.Library;
 
-import com.workintech.app.library.model.Books.Book;
-
+import java.text.Collator;
 import java.time.LocalDate;
+import java.util.Locale;
 
-public class Bill {
+public class Bill implements Comparable<Bill> {
     private LocalDate date;
     private Double fine;
     private String nameReader;
-
 
     public Bill(LocalDate date, Double fine, String nameReader) {
         this.date = date;
         this.fine = fine;
         this.nameReader = nameReader;
-
     }
 
     public LocalDate getDate() {
@@ -28,4 +26,23 @@ public class Bill {
     public String getNameReader() {
         return nameReader;
     }
+
+    @Override
+    public String toString() {
+        return "-->Bill<--" +
+                "\nDate: " + date +
+                "\nFine: " + fine +
+                "\nReader: " + nameReader;
+    }
+
+    @Override
+    public int compareTo(Bill bill) {
+        Collator collator = Collator.getInstance(new Locale("tr", "TR"));
+        int nameComparison = collator.compare(this.nameReader, bill.nameReader);
+        if (bill.date.isEqual(this.date)) {
+            return nameComparison;
+        }
+        return bill.date.compareTo(this.date);
+    }
+
 }
