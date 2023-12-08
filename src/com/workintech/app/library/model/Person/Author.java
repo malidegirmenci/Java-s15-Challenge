58c2345dd64e1dd5ee2c56cb3af4d461617ecb14
Mehmet Ali Değirmenci <86076087+malidegirmenci@users.noreply.github.com>
2023-12-08 15:11:11 +0300

    added @NotNull annotations

--- src/com/workintech/app/library/model/Person/Author.java
+++ src/com/workintech/app/library/model/Person/Author.java
@@ -1,28 +1,30 @@
 package com.workintech.app.library.model.Person;
 
-import com.workintech.app.library.Enums.Roles;
+import com.workintech.app.library.enums.Roles;
 import com.workintech.app.library.model.Books.Book;
-
+import org.jetbrains.annotations.NotNull;
 
 public class Author extends Person {
     public Author(String name) {
-        super(name,Roles.AUTHOR);
+        super(name, Roles.AUTHOR);
 
     }
+
     @Override
-    public void addBook(Book book){
-        if(getName().equals(book.getAuthor())){
-            getBooks().put(book.getID(),book);
-        }else{
+    public void addBook(@NotNull Book book) {
+        if (getName().equals(book.getAuthor())) {
+            getBooks().put(book.getID(), book);
+        } else {
             System.out.println("This is not author's book");
         }
     }
+
     @Override
-    public void removeBook(Book book){
-        if(getBooks().containsKey(book.getID())){
+    public void removeBook(@NotNull Book book) {
+        if (getBooks().containsKey(book.getID())) {
             getBooks().remove(book.getID());
-        }else{
-            System.out.println("This book is not "+getClass()+"'s list. "+"So, you can not remove");
+        } else {
+            System.out.println("This book is not " + getClass() + "'s list. " + "So, you can not remove");
         }
     }
 
@@ -31,9 +33,10 @@ public class Author extends Person {
         System.out.println("-->" + getName() + "<--");
         System.out.println("Role: " + getRole());
         System.out.println("--> Books <--");
-        int i=1;
+        int i = 1;
         for (Book book : getBooks().values()) {
-            System.out.println(i++ +". "+book.getName());
+            System.out.println(i++ + ". " + book.getName());
         }
     }
+
 }
