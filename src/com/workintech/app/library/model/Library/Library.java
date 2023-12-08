b0cd968e736eb2072eb8855b70a10470754f4d35
Mehmet Ali Değirmenci <86076087+malidegirmenci@users.noreply.github.com>
2023-12-08 15:10:09 +0300

    added getBills method and showBooks and showBills methods improved with TreeSet

--- src/com/workintech/app/library/model/Library/Library.java
+++ src/com/workintech/app/library/model/Library/Library.java
@@ -1,65 +1,79 @@
 package com.workintech.app.library.model.Library;
 
-
-import com.workintech.app.library.Enums.Status;
-import com.workintech.app.library.Interfaces.Showable;
+import com.workintech.app.library.enums.Status;
+import com.workintech.app.library.interfaces.Removable;
+import com.workintech.app.library.interfaces.Showable;
 import com.workintech.app.library.model.Books.Book;
 import com.workintech.app.library.model.Person.Reader;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.*;
 
+public class Library implements Showable, Removable {
+    private final Map<UUID, Book> books;
+    private final Map<UUID, Reader> readers;
+    private final Map<UUID, Bill> bills;
 
-public class Library implements Showable {
-    private final Map<UUID,Book> books;
-    private final Map<UUID,Reader> readers;
-    private final Map<UUID,Bill> bills;
     public Library() {
         this.books = new HashMap<>();
         this.readers = new HashMap<>();
         this.bills = new HashMap<>();
     }
 
-    public Map<UUID,Book> getBooks() {
+    public Map<UUID, Book> getBooks() {
         return books;
     }
-    public Map<UUID,Reader> getReaders() {
+    public Map<UUID, Reader> getReaders() {
         return readers;
     }
-    public void newBook(Book book){
-        books.put(book.getID(),book);
-    }
-    public void registerReader(Reader reader){
-        readers.put(reader.getID(),reader);
-    }
-
     public Map<UUID, Bill> getBills() {
         return bills;
     }
 
+    public void newBook(Book book) {
+        this.books.put(book.getID(), book);
+    }
+
+    public void registerReader(Reader reader) {
+        readers.put(reader.getID(), reader);
+    }
+
+    public void removeReader(@NotNull Reader reader){this.readers.remove(reader.getID());}
+
     @Override
-    public void showBooks(){
-        for(Book book: books.values()){
+    public void removeBook(@NotNull Book book){this.books.remove(book.getID());}
+
+
+    @Override
+    public void showBooks() {
+        Set<Book> books = new TreeSet<>(this.books.values());
+        for(Book book: books){
             book.display();
-            if(book.getStatus().equals(Status.BORROWED)){
-                System.out.println("Borrowed by "+book.getOwner().getName());
+            if (book.getStatus().equals(Status.BORROWED)) {
+                System.out.println("Borrowed by " + book.getOwner().getName());
             }
         }
     }
     @Override
-    public void showMembers(){
-        for(Reader reader: readers.values()){
+    public void showMembers() {
+        for (Reader reader : readers.values()) {
             reader.showPerson();
         }
     }
-
     @Override
-    public void showReader(Reader reader) {
+    public void showReader(@NotNull Reader reader) {
         reader.showPerson();
     }
 
     @Override
-    public void showBook(Book book) {
+    public void showBook(@NotNull Book book) {
         book.display();
     }
 
+    public void showBills() {
+        Set<Bill> bills = new TreeSet<>(this.bills.values());
+        for(Bill bill: bills){
+            System.out.println(bill);
+        }
+    }
 }
