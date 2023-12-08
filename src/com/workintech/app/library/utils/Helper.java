9cfa1008026197cd20f40f1997ba46fe7536ee6e
Mehmet Ali Değirmenci <86076087+malidegirmenci@users.noreply.github.com>
2023-12-08 15:10:55 +0300

    deleted isNull methods for added @NotNull annotations

--- src/com/workintech/app/library/utils/Helper.java
+++ src/com/workintech/app/library/utils/Helper.java
@@ -1,18 +1,14 @@
 package com.workintech.app.library.utils;
 
+import org.jetbrains.annotations.NotNull;
+
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
-public class Helper{
-    public static LocalDate convertLD(String date) {
+public class Helper {
+
+    public static LocalDate convertLD(@NotNull String date) {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
         return LocalDate.parse(date, formatter);
-    }
-
-    public static boolean isNull(String value) {
-        return value == null;
-    }
-    public static boolean isNull(Integer value) {
-        return value == null;
     }
 }
